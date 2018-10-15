744386728f84279fd2926471b673b7894f0691bf
Akshay Mourya <akshay.mourya@audetemi.com>
2018-10-15 15:54:25 +0530

    Opty Changes

---
+++ Volvo_Core/Volvo_Core/Static_Solr_url.py
@@ -0,0 +1,2 @@
+
+OPTY_DATA_URL = "OPTY/select?indent=on&q=PositionId_s:%s&wt=json"
---
+++ Volvo_Core/Volvo_Core/System_message.py
@@ -0,0 +1,9 @@
+POSITION_NOT_PROVIDED = {
+    "error_msg": "Position Not Provided",
+    "error_code": 4001
+}
+
+SOLR_RETRIVAL_FAILED = {
+    "error_msg": "Failed to Retrive from solr",
+    "error_code": 15001
+}
