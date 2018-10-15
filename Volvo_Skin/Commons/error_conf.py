7754e6409f87437c3ff20b7157698bb3b3295541
Akshay Mourya <akshay.mourya@audetemi.com>
2018-10-15 10:59:46 +0530

    Common Commit

---
+++ Volvo_Skin/Commons/error_conf.py
@@ -0,0 +1,9 @@
+POSITION_ID_NOT_PROVIDED = {
+    "msg": "Position Id Not Provided",
+    "error_code": 6001
+}
+
+PROBLEM_WITH_CORE = {
+    "msg": "Failed to process your request.",
+    "error_code": 1
+}
